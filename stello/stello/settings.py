6cd5aa7b43fa650743b969ff0b4ece9214d161ce
medetzhan1996 <67154220+medetzhan1996@users.noreply.github.com>
2020-11-03 23:41:59 +0600

    change prod settings

--- stello/stello/settings.py
+++ stello/stello/settings.py
@@ -81,17 +81,6 @@ PWA_APP_START_URL = ''
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'stello',
-        'USER': 'postgres',
-        'PASSWORD': 'dostar1996',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
